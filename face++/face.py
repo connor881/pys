6b81070f7773d14ffb0024b95d71aa52abc9009c
connor881 <7711303@qq.com>
2017-09-25 14:20:51 +0800

    add all files

---
+++ face++/face.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import urllib
+import time
+http_url='https://api-cn.faceplusplus.com/facepp/v3/detect'
+key = "填上你的KEY"
+secret = "填上你的SECRET"
+filepath = r"本地图片的路径"
+boundary = '----------%s' % hex(int(time.time() * 1000))
+data = []
+data.append('--%s' % boundary)
+data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_key')
+data.append(key)
+data.append('--%s' % boundary)
+data.append('Content-Disposition: form-data; name="%s"\r\n' % 'api_secret')
+data.append(secret)
+data.append('--%s' % boundary)
+fr=open(filepath,'rb')
+data.append('Content-Disposition: form-data; name="%s"; filename=" "' % 'image_file')
+data.append('Content-Type: %s\r\n' % 'application/octet-stream')
+data.append(fr.read())
+fr.close()
+data.append('--%s--\r\n' % boundary)
+
+http_body='\r\n'.join(data)
+#buld http request
+req=urllib2.Request(http_url)
+#header
+req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+req.add_data(http_body)
+try:
+	#req.add_header('Referer','http://remotserver.com/')
+	#post data to server
+	resp = urllib2.urlopen(req, timeout=5)
+	#get response
+	qrcont=resp.read()
+	print qrcont
+
+except urllib2.HTTPError as e:
+    print e.read()
---
+++ opencv/oc.py
@@ -0,0 +1,5 @@
+import cv2
+import numpy as np
+image = cv2.imread("2.jpg")
+cv2.imshow("444444444",image)
+cv2.waitKey(0)
